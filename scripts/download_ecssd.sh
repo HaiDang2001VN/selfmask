2513c492b2b9c3b819b7d97a9aacadd042e555ad
Hai-Dang Huynh-Lam <danghailamhuynh@gmail.com>
2023-02-19 16:47:18 +0700

    Fixed getting ECSSD dataset

--- scripts/download_ecssd.sh
+++ scripts/download_ecssd.sh
@@ -1,5 +1,5 @@
-wget https://www.cse.cuhk.edu.hk/leojia/projects/hsaliency/data/ECSSD/images.zip
-wget https://www.cse.cuhk.edu.hk/leojia/projects/hsaliency/data/ECSSD/ground_truth_mask.zip
+wget https://www.cse.cuhk.edu.hk/leojia/projects/hsaliency/data/ECSSD/images.zip --no-check-certificate
+wget https://www.cse.cuhk.edu.hk/leojia/projects/hsaliency/data/ECSSD/ground_truth_mask.zip --no-check-certificate
 
 unzip images.zip 
 unzip ground_truth_mask.zip
